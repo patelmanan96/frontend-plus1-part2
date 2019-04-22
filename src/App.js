7d252425b8ca032ed0514a14ac9d66458af84a13
Manan <patel.mana@husky.neu.edu>
2019-04-22 14:50:56 -0500

    Committing unversioned important files

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import './App.css';
+import {BrowserRouter as Router,Route,Link} from 'react-router-dom'
+import SignUp from "./components/SignUp";
+import LoginPage from "./components/LoginPage";
+import LoginSuccess from "./components/LoginSuccess";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Router>
+          <Route path='/login' component={LoginPage}/>
+          <Route path='/signUp' component={SignUp}/>
+          <Route path='/loginSuccess' component={LoginSuccess}/>
+        </Router>
+      </div>
+    );
+  }
+}
+
+export default App;
